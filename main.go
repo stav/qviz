c7a3052311dec453babf7180b8a8bb2e16bf103d
Steven Almeroth <steven.almeroth@gmail.com>
2024-03-26 17:40:03 -0400

    hello web

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
 
 func main() {
 	fmt.Println("Hello, world.")
+	helloHandler := func(w http.ResponseWriter, req *http.Request) {
+		io.WriteString(w, "Hello, world!\n")
+	}
+
+	http.HandleFunc("/hello", helloHandler)
+	log.Println("Listing for requests at http://localhost:8000/hello")
+	log.Fatal(http.ListenAndServe(":8000", nil))
 }
