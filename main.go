801e19562334fe45a3f54682383f26dbb7e444fe
Steven Almeroth <steven.almeroth@gmail.com>
2024-03-29 11:20:29 -0400

    statics

--- main.go
+++ main.go
@@ -38,6 +38,10 @@ func main() {
 		Format: "${status} ${method} ${uri} ${error}\n",
 	}))
 
+	e.File("/htmx.png", "images/htmx.png")
+	e.File("/htmx.js", "js/htmx-1.9.11.js")
+	e.File("/index.js", "js/index.js")
+
 	e.GET("/", routes.IndexHandler)
 	e.GET("/login", routes.GetLoginHandler)
 	e.POST("/login", routes.PostLoginHandler)
