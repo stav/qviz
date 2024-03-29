17a8d20ac73fba835f5b4b2e6f000a7c21b47327
Steven Almeroth <steven.almeroth@gmail.com>
2024-03-29 12:22:46 -0400

    client

--- js/index.js
+++ js/index.js
@@ -1,10 +1,15 @@
-document.addEventListener("DOMContentLoaded", (event) => {
-  console.log('htmx version:', htmx.version);
-  htmx.logAll()
+document.addEventListener('DOMContentLoaded', event => {
+  console.log('htmx version:', htmx.version)
+  // htmx.logAll()
   document.body.addEventListener('htmx:beforeSwap', function (evt) {
     if (evt.detail.xhr.status === 422) {
-      evt.detail.shouldSwap = true;
-      evt.detail.isError = false;
+      evt.detail.shouldSwap = true
+      evt.detail.isError = false
     }
   })
 })
+
+const SUPABASE_URL = 'https://---.supabase.co'
+const SUPABASE_KEY = 'eyJhbGciOiJ---Ql2hB8U9mhbJk'
+const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY)
+document.client = client
