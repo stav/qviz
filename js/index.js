ef17046bd0349cb8357e43701a0eae7cdd0755ca
Steven Almeroth <steven.almeroth@gmail.com>
2024-03-30 08:08:20 -0400

    cleanups

--- js/index.js
+++ js/index.js
@@ -1,10 +1,10 @@
-document.addEventListener("DOMContentLoaded", (event) => {
-  console.log('htmx version:', htmx.version);
-  htmx.logAll()
+document.addEventListener('DOMContentLoaded', event => {
+  console.log('htmx version:', htmx.version)
+  // htmx.logAll()
   document.body.addEventListener('htmx:beforeSwap', function (evt) {
     if (evt.detail.xhr.status === 422) {
-      evt.detail.shouldSwap = true;
-      evt.detail.isError = false;
+      evt.detail.shouldSwap = true
+      evt.detail.isError = false
     }
   })
 })
