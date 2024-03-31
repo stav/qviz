61910e00a0c411137ea600988d4b80d8535964aa
Steven Almeroth <steven.almeroth@gmail.com>
2024-03-30 21:42:05 -0400

    middleware

--- go.mod
+++ go.mod
@@ -5,12 +5,14 @@ go 1.22.0
 require github.com/labstack/echo/v4 v4.11.4
 
 require (
+	github.com/golang-jwt/jwt/v5 v5.0.0 // indirect
 	github.com/google/go-querystring v1.1.0 // indirect
 	github.com/nedpals/postgrest-go v0.1.3 // indirect
 )
 
 require (
 	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
+	github.com/labstack/echo-jwt/v4 v4.2.0
 	github.com/labstack/gommon v0.4.2 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
--- main.go
+++ main.go
@@ -1,10 +1,16 @@
 package main
 
 import (
+	"errors"
+	"fmt"
 	"html/template"
 	"io"
 	"log"
+	"net/http"
 
+	"github.com/golang-jwt/jwt/v5"
+
+	echojwt "github.com/labstack/echo-jwt/v4"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 
@@ -34,9 +40,39 @@ func main() {
 	e.Renderer = newTemplate()
 
 	// e.Use(middleware.Recover())
+	e.Use(echojwt.WithConfig(echojwt.Config{
+		SigningKey: []byte("secret"),
+		ContinueOnIgnoredError: true,
+		SuccessHandler: func(c echo.Context) {
+			fmt.Println("SuccessHandler")
+			user := c.Get("user").(*jwt.Token)
+			claims := user.Claims.(jwt.MapClaims)
+			// c.Logger().Info("User: ", claims["name"])
+			log.Println("user:::", claims["name"])
+		},
+		ErrorHandler: func(c echo.Context, err error) error {
+			fmt.Println("ErrorHandler")
+			c.JSON(http.StatusUnauthorized, map[string]string{
+				"error": err.Error(),
+			})
+			return nil
+		},
+	}))
 	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
 		Format: "${status} ${method} ${uri} ${error}\n",
 	}))
+	e.Use(func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			// Extract the credentials from HTTP request header and perform a security
+			// check
+
+			// For invalid credentials
+			// return echo.NewHTTPError(http.StatusUnauthorized, "Please provide valid credentials")
+
+			// For valid credentials call next
+			return next(c)
+		}
+	})
 
 	e.File("/htmx.png", "images/htmx.png")
 	e.File("/htmx.js", "js/htmx.js")
@@ -46,6 +82,20 @@ func main() {
 	e.POST("/login", routes.PostLoginHandler)
 	e.GET("/register", routes.GetRegisterHandler)
 	e.POST("/register", routes.PostRegisterHandler)
+
+	e.GET("/jwt", func(c echo.Context) error {
+		var user = c.Get("user")
+		fmt.Println("user::", user)
+		token, ok := user.(*jwt.Token) // by default token is stored under `user` key
+		if !ok {
+			return errors.New("JWT token missing or invalid")
+		}
+		claims, ok := token.Claims.(jwt.MapClaims) // by default claims is of type `jwt.MapClaims`
+		if !ok {
+			return errors.New("failed to cast claims as jwt.MapClaims")
+		}
+		return c.JSON(http.StatusOK, claims)
+	})
 
 	e.Logger.Fatal(e.Start(":8888"))
 
