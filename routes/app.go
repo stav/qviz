56e6808656c75f822bf59c7bcd03edfc408565e6
Steven Almeroth <steven.almeroth@gmail.com>
2024-04-03 15:23:40 -0400

    app page

--- routes/app.go
+++ routes/app.go
@@ -1,13 +1,9 @@
 package routes
 
 import (
-	"fmt"
-	"net/http"
-
 	"github.com/labstack/echo/v4"
 )
 
 func AppIndexHandler(c echo.Context) error {
-  fmt.Println("GET /app")
-  return c.JSON(http.StatusOK, "{ \"message\": \"GET /app\" }")
+	return c.Render(200, "app", "Qviz App")
 }
