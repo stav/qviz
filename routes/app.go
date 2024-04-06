9eba3121fd69b75b65b235ec8c6e4f3ab3d48ed6
Steven Almeroth <steven.almeroth@gmail.com>
2024-04-06 02:47:53 -0400

    db select

--- routes/app.go
+++ routes/app.go
@@ -4,6 +4,17 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
+type Quiz struct {
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+	Date string `json:"created_at"`
+}
+
 func AppIndexHandler(c echo.Context) error {
-	return c.Render(200, "app", "Qviz App")
+	var results []Quiz
+	err := supabase.DB.From("quiz").Select("*").Execute(&results)
+	if err != nil {
+		return c.JSON(500, err)
+	}
+	return c.Render(200, "app", results)
 }
