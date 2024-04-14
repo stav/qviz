441628cc7ee7fd0e854e7f597e4b327f86b26ad8
Steven Almeroth <steven.almeroth@gmail.com>
2024-04-14 09:40:56 -0400

    router

--- routes/index.go
+++ routes/index.go
@@ -4,10 +4,13 @@ import (
 	"os"
 
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 
 	supa "github.com/nedpals/supabase-go"
 
 	_ "github.com/joho/godotenv/autoload"
+
+	qviz_middleware "bld/qviz/middleware"
 )
 
 var SUPABASE_URL string = os.Getenv("SUPABASE_URL")
@@ -15,9 +18,9 @@ var SUPABASE_KEY string = os.Getenv("SUPABASE_KEY")
 var supabase = supa.CreateClient(SUPABASE_URL, SUPABASE_KEY)
 
 type Quiz struct {
-	ID   int    `json:"id"`
-	Name string `json:"name"`
-	Date string `json:"created_at"`
+	ID   int        `json:"id"`
+	Name string     `json:"name"`
+	Date string     `json:"created_at"`
 	Ques []Question `json:"question"`
 	Msg  string
 }
@@ -27,17 +30,51 @@ func (q *Quiz) Error() string {
 }
 
 type Question struct {
-	ID   int    `json:"id"`
-	Text string `json:"text"`
+	ID   int      `json:"id"`
+	Text string   `json:"text"`
 	Ans  []Answer `json:"answer"`
 }
 
 type Answer struct {
-	ID	 int    `json:"id"`
+	ID   int    `json:"id"`
 	Text string `json:"text"`
 	True bool   `json:"is_correct"`
 }
 
 func IndexHandler(c echo.Context) error {
 	return c.Render(200, "index.html", "Hello, Qviz!")
+}
+
+// export a function that returns an echo instance
+func NewServer() *echo.Echo {
+	e := echo.New()
+	e.Logger.Print("Logging to the echo logger")
+	e.Renderer = newTemplate()
+	// e.Use(middleware.Recover())
+	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
+		Format: "${status} ${method} ${uri} ${error}\n",
+	}))
+
+	e.File("/htmx.png", "images/htmx.png")
+	e.File("/htmx.js", "js/htmx.js", qviz_middleware.AddScriptHeader)
+	e.File("/head.js", "js/head.js", qviz_middleware.AddScriptHeader)
+
+	e.GET("/", GetLoginHandler)
+	e.GET("/login", GetLoginHandler)
+	e.POST("/login", PostLoginHandler)
+	e.POST("/logout", PostLogoutHandler)
+	e.GET("/register", GetRegisterHandler)
+	e.POST("/register", PostRegisterHandler)
+
+	app := e.Group("/app")
+	app.Use(qviz_middleware.Sentry)
+	app.GET("", AppIndexHandler)
+	app.GET("/quiz/:id", AppQuizHandler)
+
+	api := e.Group("/api")
+	api.Use(qviz_middleware.Sentry)
+	api.GET("/quiz/:id", ApiQuizHandler)
+
+	// return the echo instance
+	return e
 }
---
+++ routes/router.go
@@ -0,0 +1,29 @@
+package routes
+
+import (
+	"io"
+	"text/template"
+
+	"github.com/labstack/echo/v4"
+)
+
+type Template struct {
+	tmpl *template.Template
+}
+
+func newTemplate() *Template {
+	t := template.New("views")
+	t.Funcs(template.FuncMap{
+		"inc": increment,
+	})
+	t.ParseGlob("views/*.html")
+	return &Template{ tmpl: t }
+}
+
+func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	return t.tmpl.ExecuteTemplate(w, name, data)
+}
+
+func increment(n int) int {
+	return n + 1
+}
