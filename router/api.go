5b0468010294500462109908b664842ca1b46869
Steven Almeroth <steven.almeroth@gmail.com>
2024-04-14 17:58:57 -0400

    simple query

--- router/api.go
+++ router/api.go
@@ -11,13 +11,13 @@ func QuizFromId(c echo.Context) Quiz {
 	id := c.Param("id")
 	fmt.Println("ID:", id)
 
-	var quizQs Quiz
-	var quiz Quiz
 	var err error
+	var quiz Quiz
+	var questions []Question
 
 	// First query to get the quiz
-	// We only need this first call because I can't figure out how to outer join
-	// the questions and answers in the second call which should be the only call
+	// We should only need one db call but I can't figure out how to outer join
+	// the questions and answers in one query with nedpals/supabase-go library
 	err = supabase.DB.From("quiz").Select("*").Single().Filter("id", "eq", id).Execute(&quiz)
 	if err != nil {
 		quiz.Msg = err.Error()
@@ -26,7 +26,8 @@ func QuizFromId(c echo.Context) Quiz {
 	fmt.Println("Quiz:", quiz)
 
 	// Second query to get the questions & answers
-	err = supabase.DB.From("quiz").Select("*,question!inner(id,text,answer!left(id,text,is_correct))").Single().Filter("id", "eq", id).Execute(&quizQs)
+	query := "id,text,answer!left(id,text,is_correct)"
+	err = supabase.DB.From("question").Select(query).Filter("quiz_id", "eq", id).Execute(&questions)
 	if err != nil {
 		fmt.Println("Error:", err)
 		// Check if there are no questions
@@ -37,11 +38,11 @@ func QuizFromId(c echo.Context) Quiz {
 		quiz.Msg = err.Error()
 		return quiz
 	}
-	fmt.Println("quizQs:", quizQs)
+	fmt.Println("questions:", questions)
 
 	// Copy the questions to the quiz one at a time because of the different types
-	quiz.Ques = make([]Question, len(quizQs.Ques))
-	for i, q := range quizQs.Ques {
+	quiz.Ques = make([]Question, len(questions))
+	for i, q := range questions {
 		fmt.Println("iQ:", i, q)
 		quiz.Ques[i] = q
 	}
