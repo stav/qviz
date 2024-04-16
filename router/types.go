8bb7db77097eec14b9ea8b046de4a8313b6c11ab
Steven Almeroth <steven.almeroth@gmail.com>
2024-04-16 10:48:18 -0400

    normal types

---
+++ router/types.go
@@ -0,0 +1,27 @@
+package router
+
+// Contains the common types used in the router package
+
+type Quiz struct {
+	ID   int        `json:"id"`
+	Name string     `json:"name"`
+	Date string     `json:"created_at"`
+	Ques []Question `json:"question"`
+	Msg  string
+}
+
+func (q *Quiz) Error() string {
+	return q.Msg
+}
+
+type Question struct {
+	ID   int      `json:"id"`
+	Text string   `json:"text"`
+	Ans  []Answer `json:"answer"`
+}
+
+type Answer struct {
+	ID   int    `json:"id"`
+	Text string `json:"text"`
+	True bool   `json:"is_correct"`
+}
