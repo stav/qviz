cc167bba25eda5b2c089b8e3a1ced610afcbe3f9
Steven Almeroth <steven.almeroth@gmail.com>
2024-04-16 23:39:44 -0400

    cleanup client

--- js/head.js
+++ js/head.js
@@ -1,29 +1,5 @@
-/**
- * Set the user box to the current user
- */
-function setUserBox () {
-  const token = localStorage.getItem('supabase.auth.token')
-  if (token.length > 0) {
-    const userBox = document.getElementById('user-box')
-    if (userBox) {
-      const user = localStorage.getItem('supabase.auth.user')
-      userBox.innerHTML = user
-    }
-  }
-}
-
-/**
- * Remove the user token from local storage
- */
-function logout () {
-  localStorage.removeItem('supabase.auth.user')
-  localStorage.removeItem('supabase.auth.token')
-
-  const userBox = document.getElementById('user-box')
-  if (userBox) {
-    userBox.innerHTML = ''
-  }
-}
+const userKey = 'supabase.auth.user';
+const tokenKey = 'supabase.auth.token';
 
 /**
  * Save the user token to local storage
@@ -33,11 +9,38 @@ function logout () {
  */
 function login (user, token) {
   if (token.length > 0) {
-    localStorage.setItem('supabase.auth.user', user)
-    localStorage.setItem('supabase.auth.token', token)
+    localStorage.setItem(userKey, user)
+    localStorage.setItem(tokenKey, token)
   }
   const userBox = document.getElementById('user-box')
   if (userBox) {
     userBox.innerHTML = user
+  }
+}
+
+/**
+ * Remove the user token from local storage
+ */
+function logout () {
+  localStorage.removeItem(userKey)
+  localStorage.removeItem(tokenKey)
+
+  const userBox = document.getElementById('user-box')
+  if (userBox) {
+    userBox.innerHTML = ''
+  }
+}
+
+/**
+ * Set the user box to the current user
+ */
+function setUserBox () {
+  const token = localStorage.getItem(tokenKey)
+  if (token?.length) {
+    const userBox = document.getElementById('user-box')
+    if (userBox) {
+      const user = localStorage.getItem(userKey)
+      userBox.innerHTML = user
+    }
   }
 }
